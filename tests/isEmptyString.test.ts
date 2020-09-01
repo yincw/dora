8187e852ee532bfc11ad469251e3347175e83349
VANKE\yincw <yincw@vanke.com>
2020-09-01 13:40:39 +0800

    test: test
    test

--- tests/isEmptyString.test.ts
+++ tests/isEmptyString.test.ts
@@ -2,10 +2,9 @@ import { isEmptyString } from '../src/string/isEmptyString';
 
 // 测试套件
 describe('string/isEmptyString 函数', () => {
-    // 测试块
-    test('数据源参数必填', () => {
-        // 预期
-        // expect(isEmptyString()).toThrow();
-    });
-
+  // 测试块
+  test('数据源参数必填', () => {
+    // 预期
+    // expect(isEmptyString()).toThrow();
+  });
 });
